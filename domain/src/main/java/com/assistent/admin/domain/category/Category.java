c5581a92f7a86d3f9f01afd8cba923a8399995d6
Marcos Michel Machado Koch <locacao@MBL-CK64CFD7W5.local>
2025-07-24 17:59:16 -0300

    Comportamento de ativar e inativar uma categoria

--- domain/src/main/java/com/assistent/admin/domain/category/Category.java
+++ domain/src/main/java/com/assistent/admin/domain/category/Category.java
@@ -42,13 +42,32 @@ public class Category extends AggregateRoot<CategoryID> {
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
-    public CategoryID getId() {
-        return id;
-    }
-
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();
+    }
+
+    public Category activate() {
+        this.deletedAt = null;
+        this.active = true;
+        this.updatedAt = Instant.now();
+
+        return this;
+    }
+
+    public Category deactivate() {
+        if (getDeletedAt() == null) {
+            this.deletedAt = Instant.now();
+        }
+
+        this.active = false;
+        this.updatedAt = Instant.now();
+
+        return this;
+    }
+
+    public CategoryID getId() {
+        return id;
     }
 
     public String getName() {
