e244990cd574c26df117a80aae17426c7628bce9
Marcos Michel Machado Koch <locacao@MBL-CK64CFD7W5.local>
2025-07-29 15:40:17 -0300

    Criando o gateway de categoria

---
+++ domain/src/main/java/com/assistent/admin/domain/pagination/Pagination.java
@@ -0,0 +1,11 @@
+package com.assistent.admin.domain.pagination;
+
+import java.util.List;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> items
+) {
+}
