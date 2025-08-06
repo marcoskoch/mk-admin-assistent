8a9983ddb0d6999dc3422ecdf91a23984f5f1bcc
Marcos Michel Machado Koch <locacao@MBL-CK64CFD7W5.local>
2025-08-06 10:58:20 -0300

    Update configurations

--- infrastructure/src/test/java/com/assistent/admin/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/assistent/admin/infrastructure/MainTest.java
@@ -1,13 +1,14 @@
 package com.assistent.admin.infrastructure;
 
-import com.assistent.admin.application.UseCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
     @Test
     public void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
