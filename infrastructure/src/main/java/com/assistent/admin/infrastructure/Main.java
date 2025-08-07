64b015503a2d49cec380fa8c9006f4c37e5467e5
Marcos Michel Machado Koch <locacao@MBL-CK64CFD7W5.local>
2025-08-07 15:11:41 -0300

    feat(category): implementar repositório e gateway

--- infrastructure/src/main/java/com/assistent/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/Main.java
@@ -3,14 +3,9 @@ package com.assistent.admin.infrastructure;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-import com.assistent.admin.domain.category.Category;
-import com.assistent.admin.infrastructure.category.persistence.CategoryJpaEntity;
-import com.assistent.admin.infrastructure.category.persistence.CategoryRepository;
 import com.assistent.admin.infrastructure.configuration.WebServerConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 import java.util.List;
@@ -21,18 +16,5 @@ public class Main {
         System.out.println("Hello world!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
-    }
-
-    @Bean
-    public ApplicationRunner runner(CategoryRepository repository) {
-        return args -> {
-            List<CategoryJpaEntity> all = repository.findAll();
-
-            Category filmes = Category.newCategory("Filmes", null, true);
-
-            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
-
-            repository.deleteAll();
-        };
     }
 }
---
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/category/CategoryMySQLGateway.java
@@ -0,0 +1,44 @@
+package com.assistent.admin.infrastructure.category;
+
+import com.assistent.admin.domain.category.Category;
+import com.assistent.admin.domain.category.CategoryGateway;
+import com.assistent.admin.domain.category.CategoryID;
+import com.assistent.admin.domain.category.CategorySearchQuery;
+import com.assistent.admin.domain.pagination.Pagination;
+import com.assistent.admin.infrastructure.category.persistence.CategoryRepository;
+
+import java.util.Optional;
+
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryRepository repository;
+
+    public CategoryMySQLGateway(final CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
