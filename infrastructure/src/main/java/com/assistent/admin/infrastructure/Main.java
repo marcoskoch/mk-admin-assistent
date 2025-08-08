2df9d0af7bf56b30515bdd1440b7a4743a1cc531
Marcos Koch <marcos_koch@hotmail.com>
2025-08-08 15:23:29 -0300

    feat: integrar Springdoc OpenAPI para documentação da API

--- infrastructure/src/main/java/com/assistent/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/Main.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
 
-import java.util.List;
-
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
--- infrastructure/src/main/java/com/assistent/admin/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/api/controllers/CategoryController.java
@@ -3,7 +3,9 @@ package com.assistent.admin.infrastructure.api.controllers;
 import com.assistent.admin.domain.pagination.Pagination;
 import com.assistent.admin.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CategoryController implements CategoryAPI {
 
     @Override
