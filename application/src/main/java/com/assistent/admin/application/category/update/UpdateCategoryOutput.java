68d6bbd691335a6ee4d2fd2b9034591f90d73ebc
Marcos Koch <marcos_koch@hotmail.com>
2025-08-12 12:08:17 -0300

    test: testes integrados do endpoint de atualização de categoria

--- application/src/main/java/com/assistent/admin/application/category/update/UpdateCategoryOutput.java
+++ application/src/main/java/com/assistent/admin/application/category/update/UpdateCategoryOutput.java
@@ -4,10 +4,14 @@ import com.assistent.admin.domain.category.Category;
 import com.assistent.admin.domain.category.CategoryID;
 
 public record UpdateCategoryOutput(
-        CategoryID id
+        String id
 ) {
 
+    public static UpdateCategoryOutput from(final String anId) {
+        return new UpdateCategoryOutput(anId);
+    }
+
     public static UpdateCategoryOutput from(final Category aCategory) {
-        return new UpdateCategoryOutput(aCategory.getId());
+        return new UpdateCategoryOutput(aCategory.getId().getValue());
     }
 }
--- infrastructure/src/test/java/com/assistent/admin/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/assistent/admin/infrastructure/api/CategoryAPITest.java
@@ -5,6 +5,8 @@ import com.assistent.admin.application.category.create.CreateCategoryOutput;
 import com.assistent.admin.application.category.create.CreateCategoryUseCase;
 import com.assistent.admin.application.category.retrieve.get.CategoryOutput;
 import com.assistent.admin.application.category.retrieve.get.GetCategoryByIdUseCase;
+import com.assistent.admin.application.category.update.UpdateCategoryOutput;
+import com.assistent.admin.application.category.update.UpdateCategoryUseCase;
 import com.assistent.admin.domain.category.Category;
 import com.assistent.admin.domain.category.CategoryID;
 import com.assistent.admin.domain.exceptions.DomainException;
@@ -12,6 +14,7 @@ import com.assistent.admin.domain.exceptions.NotFoundException;
 import com.assistent.admin.domain.validation.Error;
 import com.assistent.admin.domain.validation.handler.Notification;
 import com.assistent.admin.infrastructure.category.models.CreateCategoryApiInput;
+import com.assistent.admin.infrastructure.category.models.UpdateCategoryApiInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +31,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -45,6 +49,9 @@ public class CategoryAPITest {
 
     @MockBean
     private GetCategoryByIdUseCase getCategoryByIdUseCase;
+
+    @MockBean
+    private UpdateCategoryUseCase updateCategoryUseCase;
 
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
@@ -210,5 +217,39 @@ public class CategoryAPITest {
         // then
         response.andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message", equalTo(expectedErrorMessage)));
+    }
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() throws Exception {
+        // given
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        when(updateCategoryUseCase.execute(any()))
+                .thenReturn(Right(UpdateCategoryOutput.from(expectedId)));
+
+        final var aCommand =
+                new UpdateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        // when
+        final var request = put("/categories/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(aCommand));
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(status().isNoContent())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE));
+
+        verify(updateCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
     }
 }
