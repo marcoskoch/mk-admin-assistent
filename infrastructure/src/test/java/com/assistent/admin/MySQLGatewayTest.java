0e0ba6f7b27ec6f9a8af1425e79e2cd3a6c5f2df
Marcos Koch <marcos_koch@hotmail.com>
2025-08-15 13:08:20 -0300

    test(infrastructure): adicionar testes de desserialização para CreateCategoryRequest

--- infrastructure/src/test/java/com/assistent/admin/MySQLGatewayTest.java
+++ infrastructure/src/test/java/com/assistent/admin/MySQLGatewayTest.java
@@ -1,17 +1,12 @@
 package com.assistent.admin;
 
-import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.*;
-import java.util.Collection;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -21,6 +16,6 @@ import java.util.Collection;
         @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]")
 })
 @DataJpaTest
-@ExtendWith(CleanUpExtension.class)
+@ExtendWith(MySQLCleanUpExtension.class)
 public @interface MySQLGatewayTest {
 }
