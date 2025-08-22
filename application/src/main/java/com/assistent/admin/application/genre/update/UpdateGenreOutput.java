79f77507dcdf2c55c29bf638a8d002c34288e6b2
Marcos Koch <marcos_koch@hotmail.com>
2025-08-22 15:51:33 -0300

    feat(application): implementar caso de uso de atualização de categoria

---
+++ application/src/main/java/com/assistent/admin/application/genre/update/UpdateGenreOutput.java
@@ -0,0 +1,10 @@
+package com.assistent.admin.application.genre.update;
+
+import com.assistent.admin.domain.genre.Genre;
+
+public record UpdateGenreOutput(String id) {
+
+    public static UpdateGenreOutput from(final Genre aGenre) {
+        return new UpdateGenreOutput(aGenre.getId().getValue());
+    }
+}
---
+++ application/src/main/java/com/assistent/admin/application/genre/update/UpdateGenreUseCase.java
@@ -0,0 +1,7 @@
+package com.assistent.admin.application.genre.update;
+
+import com.assistent.admin.application.UseCase;
+
+public abstract class UpdateGenreUseCase
+        extends UseCase<UpdateGenreCommand, UpdateGenreOutput> {
+}
