7059320d04ece0fde55725c706b0bdcb6c738c34
Marcos Koch <marcos_koch@hotmail.com>
2025-09-02 17:44:45 -0300

    feat(infrastructure): implementar método create no GenreGateway

--- infrastructure/src/main/java/com/assistent/admin/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/genre/GenreMySQLGateway.java
@@ -5,16 +5,25 @@ import com.assistent.admin.domain.genre.GenreGateway;
 import com.assistent.admin.domain.genre.GenreID;
 import com.assistent.admin.domain.pagination.Pagination;
 import com.assistent.admin.domain.pagination.SearchQuery;
+import com.assistent.admin.infrastructure.genre.persistence.GenreJpaEntity;
+import com.assistent.admin.infrastructure.genre.persistence.GenreRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
 public class GenreMySQLGateway implements GenreGateway {
 
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
     @Override
-    public Genre create(Genre aGenre) {
-        return null;
+    public Genre create(final Genre aGenre) {
+        return save(aGenre);
     }
 
     @Override
@@ -35,5 +44,10 @@ public class GenreMySQLGateway implements GenreGateway {
     @Override
     public Pagination<Genre> findAll(SearchQuery aQuery) {
         return null;
+    }
+
+    private Genre save(final Genre aGenre) {
+        return this.genreRepository.save(GenreJpaEntity.from(aGenre))
+                .toAggregate();
     }
 }
