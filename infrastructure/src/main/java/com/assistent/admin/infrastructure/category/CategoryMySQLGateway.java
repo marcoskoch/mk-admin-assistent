ef3b233a7a2067d03a55ae9b35793bfd688045cd
Marcos Koch <marcos_koch@hotmail.com>
2025-09-03 14:14:24 -0300

    test: cobrindo-de-testes-integrados-do-caso-de-uso-de-criacao-de-genero

--- infrastructure/src/main/java/com/assistent/admin/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/category/CategoryMySQLGateway.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 import static com.assistent.admin.infrastructure.utils.SpecificationUtils.like;
 
@@ -78,9 +79,13 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public List<CategoryID> existsByIds(final Iterable<CategoryID> ids) {
-        // TODO: Implementar quando chegar na camada de infraestrutura de Genre.
-        return Collections.emptyList();
+    public List<CategoryID> existsByIds(final Iterable<CategoryID> categoryIDs) {
+        final var ids = StreamSupport.stream(categoryIDs.spliterator(), false)
+                .map(CategoryID::getValue)
+                .toList();
+        return this.repository.existsByIds(ids).stream()
+                .map(CategoryID::from)
+                .toList();
     }
 
     private Category save(final Category aCategory) {
--- infrastructure/src/main/java/com/assistent/admin/infrastructure/category/persistence/CategoryJpaEntity.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/category/persistence/CategoryJpaEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.Instant;
 
-@Entity
+@Entity(name = "Category")
 @Table(name = "category")
 public class CategoryJpaEntity {
 
