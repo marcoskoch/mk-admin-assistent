c1fded32cec9f4729a5f9c1e6f6f9e139a31fc34
Marcos Koch <marcos_koch@hotmail.com>
2025-10-06 14:20:07 -0300

    injetando-a-implementacao-do-gateway-nos-casos-de-uso

---
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/configuration/usecases/CastMemberUseCaseConfig.java
@@ -0,0 +1,52 @@
+package com.assistent.admin.infrastructure.configuration.usecases;
+
+import com.assistent.admin.application.castmember.create.CreateCastMemberUseCase;
+import com.assistent.admin.application.castmember.create.DefaultCreateCastMemberUseCase;
+import com.assistent.admin.application.castmember.delete.DefaultDeleteCastMemberUseCase;
+import com.assistent.admin.application.castmember.delete.DeleteCastMemberUseCase;
+import com.assistent.admin.application.castmember.retrieve.get.DefaultGetCastMemberByIdUseCase;
+import com.assistent.admin.application.castmember.retrieve.get.GetCastMemberByIdUseCase;
+import com.assistent.admin.application.castmember.retrieve.list.DefaultListCastMembersUseCase;
+import com.assistent.admin.application.castmember.retrieve.list.ListCastMembersUseCase;
+import com.assistent.admin.application.castmember.update.DefaultUpdateCastMemberUseCase;
+import com.assistent.admin.application.castmember.update.UpdateCastMemberUseCase;
+import com.assistent.admin.domain.castmember.CastMemberGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class CastMemberUseCaseConfig {
+
+    private final CastMemberGateway castMemberGateway;
+
+    public CastMemberUseCaseConfig(final CastMemberGateway castMemberGateway) {
+        this.castMemberGateway = Objects.requireNonNull(castMemberGateway);
+    }
+
+    @Bean
+    public CreateCastMemberUseCase createCastMemberUseCase() {
+        return new DefaultCreateCastMemberUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public DeleteCastMemberUseCase deleteCastMemberUseCase() {
+        return new DefaultDeleteCastMemberUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public GetCastMemberByIdUseCase getCastMemberByIdUseCase() {
+        return new DefaultGetCastMemberByIdUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public ListCastMembersUseCase listCastMembersUseCase() {
+        return new DefaultListCastMembersUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public UpdateCastMemberUseCase updateCastMemberUseCase() {
+        return new DefaultUpdateCastMemberUseCase(castMemberGateway);
+    }
+}
