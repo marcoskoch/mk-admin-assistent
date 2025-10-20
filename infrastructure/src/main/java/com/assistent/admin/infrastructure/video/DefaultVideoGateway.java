0b0ad4a2facbc0ccb29b9f6fde828b87894ccdfc
Marcos Koch <marcos_koch@hotmail.com>
2025-10-20 17:15:36 -0300

    feat: implementando-o-metodo-create-do-gateway-de-video

---
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/video/DefaultVideoGateway.java
@@ -0,0 +1,49 @@
+package com.assistent.admin.infrastructure.video;
+
+import com.assistent.admin.domain.pagination.Pagination;
+import com.assistent.admin.domain.video.Video;
+import com.assistent.admin.domain.video.VideoGateway;
+import com.assistent.admin.domain.video.VideoID;
+import com.assistent.admin.domain.video.VideoSearchQuery;
+import com.assistent.admin.infrastructure.video.persistence.VideoJpaEntity;
+import com.assistent.admin.infrastructure.video.persistence.VideoRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class DefaultVideoGateway implements VideoGateway {
+
+    private final VideoRepository videoRepository;
+
+    public DefaultVideoGateway(final VideoRepository videoRepository) {
+        this.videoRepository = Objects.requireNonNull(videoRepository);
+    }
+
+    @Override
+    @Transactional
+    public Video create(final Video aVideo) {
+        return this.videoRepository.save(VideoJpaEntity.from(aVideo))
+                .toAggregate();
+    }
+
+    @Override
+    public void deleteById(VideoID anId) {
+
+    }
+
+    @Override
+    public Optional<Video> findById(VideoID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Video update(Video aVideo) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Video> findAll(VideoSearchQuery aQuery) {
+        return null;
+    }
+}
