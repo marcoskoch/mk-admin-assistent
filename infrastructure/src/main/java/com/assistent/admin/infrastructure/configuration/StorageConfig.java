f5b4f7516c02fbb6870cca9e5d943d876a598880
Marcos Koch <marcos_koch@hotmail.com>
2025-11-08 17:04:45 -0300

    feat: implementando Media Resource Gateway

--- infrastructure/src/main/java/com/assistent/admin/infrastructure/configuration/StorageConfig.java
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/configuration/StorageConfig.java
@@ -1,17 +1,25 @@
 package com.assistent.admin.infrastructure.configuration;
 
 import com.assistent.admin.infrastructure.configuration.properties.google.GoogleStorageProperties;
+import com.assistent.admin.infrastructure.configuration.properties.storage.StorageProperties;
 import com.assistent.admin.infrastructure.services.StorageService;
 import com.assistent.admin.infrastructure.services.impl.GCStorageService;
 import com.assistent.admin.infrastructure.services.local.InMemoryStorageService;
 import com.google.cloud.storage.Storage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class StorageConfig {
+
+    @Bean
+    @ConfigurationProperties(value = "storage.catalogo-videos")
+    public StorageProperties storageProperties() {
+        return new StorageProperties();
+    }
 
     @Bean(name = "StorageService")
     @Profile({"production", "development"})
