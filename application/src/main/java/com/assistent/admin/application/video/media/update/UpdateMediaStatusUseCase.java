bac2eaddb3df368351f51949b10d9c52ae45027e
Marcos Koch <marcos_koch@hotmail.com>
2025-12-02 17:42:09 -0300

    feat: implementando caso de uso de update media status

---
+++ application/src/main/java/com/assistent/admin/application/video/media/update/UpdateMediaStatusUseCase.java
@@ -0,0 +1,6 @@
+package com.assistent.admin.application.video.media.update;
+
+import com.assistent.admin.application.UnitUseCase;
+
+public abstract class UpdateMediaStatusUseCase extends UnitUseCase<UpdateMediaStatusCommand> {
+}
