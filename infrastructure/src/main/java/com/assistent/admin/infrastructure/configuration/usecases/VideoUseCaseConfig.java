d429afafe6f1d0831601c2646e8edd0944864a0d
Marcos Koch <marcos_koch@hotmail.com>
2026-03-02 17:29:50 -0300

    26.05-configuracao-dos-usecases-de-video-media

---
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/configuration/usecases/VideoUseCaseConfig.java
@@ -0,0 +1,24 @@
+package com.assistent.admin.infrastructure.configuration.usecases;
+
+import com.assistent.admin.application.video.media.update.DefaultUpdateMediaStatusUseCase;
+import com.assistent.admin.application.video.media.update.UpdateMediaStatusUseCase;
+import com.assistent.admin.domain.video.VideoGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class VideoUseCaseConfig {
+
+    private final VideoGateway videoGateway;
+
+    public VideoUseCaseConfig(final VideoGateway videoGateway) {
+        this.videoGateway = Objects.requireNonNull(videoGateway);
+    }
+
+    @Bean
+    public UpdateMediaStatusUseCase updateMediaStatusUseCase() {
+        return new DefaultUpdateMediaStatusUseCase(videoGateway);
+    }
+}
