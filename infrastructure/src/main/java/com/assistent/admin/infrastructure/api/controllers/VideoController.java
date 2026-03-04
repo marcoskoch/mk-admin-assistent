d505c8969b5e00bac7fcb801fd811e9461c5157b
Marcos Koch <marcos_koch@hotmail.com>
2026-03-04 17:23:21 -0300

    27.02-testes-integrados-do-endpoint-de-criacao-de-video-full

---
+++ infrastructure/src/main/java/com/assistent/admin/infrastructure/api/controllers/VideoController.java
@@ -0,0 +1,8 @@
+package com.assistent.admin.infrastructure.api.controllers;
+
+import com.assistent.admin.infrastructure.api.VideoAPI;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController implements VideoAPI {
+}
